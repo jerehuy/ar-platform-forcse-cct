df90b6f63b368c6157f6e9c3f1859ac1815a7f44
Camilla Selkälä <camiselkala@gmail.com>
2020-11-27 10:57:18 +0200

    Marker can show current location coordinates

--- frontend/src/components/Maps.js
+++ frontend/src/components/Maps.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -16,57 +16,47 @@ Leaflet.Marker.prototype.options.icon = Leaflet.icon({
   iconAnchor: [12, 41]
 });
 
-
-/*function markerFinder() {
-    var marker = document.getElementById("marker");
-    marker.on('dragend', function(event) {
-    var marker = event.target;  // you could also simply access the marker through the closure
-    var result = marker.getLatLng();  // but using the passed event is cleaner
-    console.log(result);
-    this.setState({position: result});
-});}*/
-
-class Amap extends Component {
+function Amap(props) {
 
     //Here you can decide how big the map will be
-    mapSize = {
+    const mapSize = {
         height: "400px", 
         width: "400px"
     }
-    constructor (props) {
-        super(props);
-        this.state = {position: [51.505, -0.09]}
-        //this.markerHandler = this.markerHandler.bind(this);
-    }
-        
-    /*markerHandler(e) {
-        let latlng = e.target._latlng;
-        this.setState({
-            position: latlng
+
+    //use coordinates you want as the center of the map as default
+    //in format [Latitude, longtitude]
+    const [position, setPosition] = useState([51.505, -0.09])
+
+    function markerFinder() {
+        var marker = document.getElementById("marker");
+        marker.on('dragend', function(event) {
+            var marker = event.target;  // you could also simply access the marker through the closure
+            var result = marker.getLatLng();  // but using the passed event is cleaner
+            console.log(result);
+            setPosition(result);
         });
-    }*/
-
-    render() {
-        return (
-            <MapContainer 
-              center={[51.505, -0.09]} 
-              zoom={13} 
-              scrollWheelZoom={false} 
-              style = {this.mapSize}>
-
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={this.state.position} draggable={true} /*eventHandlers={markerFinder()} id="marker"*/>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable. <br />
-                    Position <pre>{JSON.stringify(this.state.position, null, 2)}</pre>
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        );
     }
+
+    return (
+        <MapContainer 
+            center={[51.505, -0.09]} 
+            zoom={13} 
+            scrollWheelZoom={false} 
+            style = {mapSize}>
+
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position} draggable={true} eventHandlers={markerFinder()} id='marker'>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable. <br />
+                Position <pre>{JSON.stringify(position, null, 2)}</pre>
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
 }
 
 export default Amap;
