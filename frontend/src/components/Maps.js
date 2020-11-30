3d010e90362cf87b028c93100a0de94e2fa32691
Camilla Selkälä <camiselkala@gmail.com>
2020-11-30 20:10:49 +0200

    Map working and added fields for coordinate component adding

--- frontend/src/components/Maps.js
+++ frontend/src/components/Maps.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet';
 import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -21,41 +21,41 @@ function Amap(props) {
     //Here you can decide how big the map will be
     const mapSize = {
         height: "400px", 
-        width: "400px"
+        width: "600px"
     }
 
-    //use coordinates you want as the center of the map as default
-    //in format [Latitude, longtitude]
-    const [position, setPosition] = useState([51.505, -0.09])
-
-    function markerFinder() {
-        var marker = document.getElementById("marker");
-        marker.on('dragend', function(event) {
-            var marker = event.target;  // you could also simply access the marker through the closure
-            var result = marker.getLatLng();  // but using the passed event is cleaner
-            console.log(result);
-            setPosition(result);
+    function MyMarker() {
+      
+        const map = useMapEvents({
+          click: (e) => {
+            const { lat, lng } = e.latlng;
+            props.onChange([lat, lng]);
+          }
         });
-    }
+        
+        return (
+          <Marker position={props.position} draggable={true}>
+            <Popup>
+                Position <pre>{JSON.stringify(props.position, null, 2)}</pre>
+            </Popup>
+          </Marker>
+        )
+      }
 
     return (
-        <MapContainer 
-            center={[51.505, -0.09]} 
-            zoom={13} 
-            scrollWheelZoom={false} 
-            style = {mapSize}>
-
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position} draggable={true} eventHandlers={markerFinder()} id='marker'>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable. <br />
-                Position <pre>{JSON.stringify(position, null, 2)}</pre>
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div>
+            <MapContainer 
+              center={[51.505, -0.09]} 
+              zoom={13} 
+              scrollWheelZoom={false} 
+              style = {mapSize}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <MyMarker />
+            </MapContainer>
+        </div>
     );
 }
 
--- frontend/src/components/add-coordinate.component.js
+++ frontend/src/components/add-coordinate.component.js
@@ -55,21 +55,44 @@ export default function AddCoordinate(props) {
     })
   }
 
+  const [position, setPosition] = useState([51.505, -0.09])
+  function handleMapChange(newPosition) {
+    setPosition(newPosition);
+  }
+
   return (
     <div>
-      <ExampleMap />
       <h4>Add new coordinates</h4>
+      <Amap position={position} onChange={handleMapChange} />
+      <p>
+        Position of the marker <br />
+        Latitude: {position[0]}, Longtitude: {position[1]} <br/>
+        Notice: Position of the marker changes when you click on the map!
+      </p>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label>Latitude</label>
-          <input type="text" required className="form-control" value={data.latitude} onChange={e => setData({...data, latitude: e.target.value})}/>
+          <label htmlFor="lat">Latitude</label>
+          <input type="text" id="lat" required className="form-control" value={position[0]} onChange={e => setData({...data, latitude: e.target.value})}/>
         </div>
         <div className="form-group">
-          <label>Longitude</label>
-          <input type="text" required className="form-control" value={data.longitude} onChange={e => setData({...data, longitude: e.target.value})}/>
+          <label htmlFor="lng">Longitude</label>
+          <input type="text" id="lng" required className="form-control" value={position[1]} onChange={e => setData({...data, longitude: e.target.value})}/>
         </div>
         <div className="form-group">
-          <input type="file" required accept='.mp3' onChange={e => setData({...data, audio: e.target.files[0]})} />
+          <label htmlFor="rad">Radius (meters?)</label>
+          <input type="number" id="rad" defaultValue={data.radius} className="form-control" onChange={e => setData({...data, radius: e.target.value})}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="act">Activation (seconds?)</label>
+          <input type="number" id="act" defaultValue={data.activation} className="form-control" onChange={e => setData({...data, activation: e.target.value})}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="dact">Deactivation (seconds?)</label>
+          <input type="number" id="dact" defaultValue={data.deactivation} className="form-control" onChange={e => setData({...data, deactivation: e.target.value})}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="audio">Add audio file</label>
+          <input type="file" id="audio" required accept='.mp3' onChange={e => setData({...data, audio: e.target.files[0]})} />
         </div>
         { errors.showError
           ? <label style={{color: "red"}}>{errors.errorMsg}</label>
